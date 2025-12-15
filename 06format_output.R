1abca02a3be5c5f79ef3cf2c7e7854e89660f222
Claudia Krueger <ckrueger2@luc.edu>
2025-12-15 12:24:15 -0600

    ouput naming update

--- 06format_output.R
+++ 06format_output.R
@@ -9,8 +9,14 @@ parser$add_argument("--phecode", help="all of us phenotype ID")
 args <- parser$parse_args()
 
 #build file read in patterns - updated for new naming convention
-pattern <- paste0("/home/jupyter/gwas_*_db_*_predixcan_output_", args$phecode, "_*.csv")
+pattern <- paste0(
+  "/home/jupyter/gwas_*_db_*_predixcan_output_",
+  args$phecode,
+  "_*.csv"
+)
+
 files <- Sys.glob(pattern)
+
 cat("Number of files found:", length(files), "\n\n")
 
 #initialize empty list
